75910bdb35dc69f79cb7df3f79329a8391114539
heralt-vernandes <geraldibrani25@gmail.com>
2026-02-27 10:21:28 +0700

    Update website.sh for PHP 8.1 installation

--- website.sh
+++ website.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+set -e
+
+echo "=== INSTALL APACHE + PHP 8.1 (Ubuntu 22) ==="
 
 apt update -y
-apt install -y apache2 php libapache2-mod-php php-cli php-mysql php-gd php-xml php-curl php-mbstring php-intl php-zip php-soap php-bcmath wget
+apt install -y apache2 php8.1 libapache2-mod-php8.1 php8.1-cli php8.1-mysql php8.1-gd php8.1-xml php8.1-curl php8.1-mbstring php8.1-intl php8.1-zip php8.1-soap php8.1-bcmath wget
 
 systemctl enable apache2
 systemctl start apache2
@@ -14,13 +17,14 @@ rm moodle-latest-405.tgz
 
 mkdir -p /var/www/moodledata
 
-chown -R www-data:www-data /var/www/moodledata/
-chmod -R 755 /var/www/moodledata/
+chown -R www-data:www-data /var/www/moodledata
+chmod -R 775 /var/www/moodledata
 
-chown -R www-data:www-data moodle
-chmod -R 755 moodle
+chown -R www-data:www-data /var/www/html/moodle
+chmod -R 755 /var/www/html/moodle
 
-PHPINI=$(php -i | grep "Loaded Configuration File" | awk '{print $5}')
+# Configure PHP 8.1
+PHPINI="/etc/php/8.1/apache2/php.ini"
 
 sed -i "s/^max_input_vars.*/max_input_vars = 5000/" $PHPINI
 sed -i "s/^memory_limit.*/memory_limit = 512M/" $PHPINI
